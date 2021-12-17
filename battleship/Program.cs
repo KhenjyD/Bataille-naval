3b5db48f1f240f1f84fc0fe1ca143e43a462a246
Khenjy DONAT <khenkyr@msn.com>
2021-12-17 14:16:44 +0100

    initialisation projet

---
+++ battleship/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace battleship
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] map = new int[10, 10]; 
+        }
+    }
+}
