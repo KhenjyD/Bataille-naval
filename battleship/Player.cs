7c1460d11f2d14086106ada07a299ec667ff6536
Khenjy DONAT <khenkyr@msn.com>
2021-12-20 14:28:07 +0100

    création de la classe joueur et de ses actions

---
+++ battleship/Player.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace battleship
+{
+    class Player
+    {
+        public int id;
+        public List<Ship> playerShip;
+        public Map playerMap;
+        public Map enemyMap;
+
+        public Player(int id, List<Ship> playerShip, Map playerMap, Map enemyMap)
+        {
+            this.id = id;
+            this.playerShip = playerShip;
+            this.playerMap = playerMap;
+            this.enemyMap = enemyMap;
+        }
+
+        /* Vérifiez si les coordonnées d'attaque sont correct et si une case a déjà été attaquer */
+        public bool VerifyCoordAttack(int coordX, int coordY)
+        {
+            bool available;
+
+            if(enemyMap.map[coordX, coordY] == 0 || enemyMap.map[coordX, coordY] == 1)
+            {
+                available = true;
+            }
+            else
+            {
+                available = false;
+            }
+
+            return available;
+        }
+
+        /* Attaquer les navires du  joueur adverse */
+        public void AttackEnemyShip()
+        {
+            int coordX, coordY;
+            byte[] ascii;
+
+            while(true)
+            {
+                try
+                {
+                    Console.WriteLine("Entrez coordonnées pour attaqué un navire.");
+                    Console.WriteLine("Entrez coordonné ligne:");
+                    coordX = int.Parse(Console.ReadLine());
+                    coordX--;
+
+                    Console.WriteLine("Entrez coordonné colonne:");
+                    ascii = Encoding.ASCII.GetBytes(Console.ReadLine().ToLower());
+                    coordY = ascii[0] - 97;
+
+                    if (!VerifyCoordAttack(coordX, coordY))
+                    {
+                        Console.WriteLine("Cette case est hors de la carte ou a déjà été attaquer.");
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Coordonnées erronées. Recommencer.");
+                }
+            }
+
+            if (enemyMap.map[coordX, coordY] == 1)
+            {
+                enemyMap.map[coordX, coordY] = -1;
+            }
+            else
+            {
+                enemyMap.map[coordX, coordY] = 2;
+            }
+        }
+
+        /* Afficher la carte du joueur */
+        public void DisplayPlayerMap()
+        {
+            Console.WriteLine("    A  B  C  D  E  F  G  H  I  J");
+            for (int row = 0; row < 10; row++)
+            {
+                if (row == 9)
+                {
+                    Console.Write((row + 1) + " ");
+                }
+                else
+                {
+                    Console.Write(" " + (row + 1) + " ");
+                }
+
+                for (int colomn = 0; colomn < 10; colomn++)
+                {
+                    if (playerMap.map[row, colomn] == 0)
+                    {
+                        Console.Write("[ ]");
+                    }
+                    else if (playerMap.map[row, colomn] == 1)
+                    {
+                        Console.Write("[#]");
+                    }
+                    else if (playerMap.map[row, colomn] == -1)
+                    {
+                        Console.Write("[x]");
+                    }
+                    else
+                    {
+                        Console.Write("[~]");
+                    }
+                }
+                Console.Write("\n");
+            }
+        }
+
+        /* Afficher la carte du joueur adverse */
+        public void DisplayEnemyMap()
+        {
+            Console.WriteLine("    A  B  C  D  E  F  G  H  I  J");
+            for (int row = 0; row < 10; row++)
+            {
+                if (row == 9)
+                {
+                    Console.Write((row + 1) + " ");
+                }
+                else
+                {
+                    Console.Write(" " + (row + 1) + " ");
+                }
+
+                for (int colomn = 0; colomn < 10; colomn++)
+                {
+                    if (enemyMap.map[row, colomn] == 0 || enemyMap.map[row, colomn] == 1)
+                    {
+                        Console.Write("[ ]");
+                    }
+                    else if (enemyMap.map[row, colomn] == -1)
+                    {
+                        Console.Write("[x]");
+                    }
+                    else if(enemyMap.map[row, colomn] == 2)
+                    {
+                        Console.Write("[~]");
+                    }
+                }
+                Console.Write("\n");
+            }
+        }
+
+        /* Créer et place les navires du joueur sur sa carte*/
+        public void InitPlayerShipOnMap()
+        {
+            List<int> shipSize = new List<int> { 2 }; //{ 2, 3, 4, 6 }
+            int coordX, coordY, direction, size;
+            byte[] ascii;
+
+            DisplayPlayerMap();
+            Console.WriteLine("Placer vos 4 navires sur les cases disponibles de taille 2, 3, 4 et 6 de long:");
+            for (int nShip = 0; nShip < 1; nShip++) //nship = 4
+            {
+                while (true)
+                {
+                    Console.WriteLine("Choisir navire par sa taille:");
+                    try
+                    {
+                        size = int.Parse(Console.ReadLine());
+
+                        if (!shipSize.Contains(size))
+                        {
+                            Console.WriteLine("Navire déjà placé ou inexistant.");
+                        }
+                        else
+                        {
+                            shipSize.Remove(size);
+                            break;
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("Navire déjà placé ou inexistant.");
+                    }
+
+                }
+
+                while (true)
+                {
+                    try
+                    {
+                        Console.WriteLine("Choisir Ligne:");
+                        coordX = int.Parse(Console.ReadLine());
+                        coordX--;
+
+                        Console.WriteLine("Choisir Colonne:");
+                        ascii = Encoding.ASCII.GetBytes(Console.ReadLine().ToLower());
+                        coordY = ascii[0] - 97;
+
+                        Console.WriteLine("Choisir Direction (Vertical = 0, Horizontal = 1):");
+                        direction = int.Parse(Console.ReadLine());
+
+                        if (!playerMap.VerifyCoordOnMap(coordX, coordY, direction, size))
+                        {
+                            Console.WriteLine("Le navire ne peut pas être placé à ces coordonnées.");
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("Coordonnées erronées. Recommencer.");
+                    }
+                }
+
+
+                Ship newShip = new Ship(size, direction, nShip);
+                playerMap.PutShipOnMap(newShip, coordX, coordY);
+
+                playerShip.Add(newShip);
+                DisplayPlayerMap();
+            }
+        }
+    }
+}
--- battleship/Program.cs
+++ battleship/Program.cs
@@ -9,77 +9,20 @@ namespace battleship
 
         static void Main(string[] args)
         {
-            Map map = new Map(new int[10, 10]);
-            List<Ship> allShip = new List<Ship>();
-            List<int> shipSize = new List<int> { 2, 3, 4, 6 };
-            int coordX, coordY, direction, size;
-            byte[] ascii;
-            map.InitMap();
-            map.DisplayMap();
+            Map playerOneMap = new Map(new int[10, 10]);
+            Map playerTwoMap = new Map(new int[10, 10]);
+            playerOneMap.InitMap();
+            playerTwoMap.InitMap();
+            Player playerOne = new Player(1, new List<Ship>(), playerOneMap, playerTwoMap);
+            Player playerTwo = new Player(1, new List<Ship>(), playerTwoMap, playerOneMap);
+            playerOne.InitPlayerShipOnMap();
+            playerTwo.InitPlayerShipOnMap();
 
-            Console.WriteLine("Placer vos 4 navires sur les cases disponibles de taille 2, 3, 4 et 6 de long:");
-            for (int nShip = 0; nShip < 4; nShip++)
+            while (true)
             {
-                while(true)
-                {
-                    Console.WriteLine("Choisir navire par sa taille:");
-                    try
-                    {
-                        size = int.Parse(Console.ReadLine());
-
-                        if (!shipSize.Contains(size))
-                        {
-                            Console.WriteLine("Navire déjà placé ou inexistant.");
-                        }
-                        else
-                        {
-                            shipSize.Remove(size);
-                            break;
-                        }
-                    }
-                    catch (Exception)
-                    {
-                        Console.WriteLine("Navire déjà placé ou inexistant.");
-                    }
-                    
-                }
-
-                while (true)
-                {
-                    try
-                    {
-                        Console.WriteLine("Choisir Ligne:");
-                        coordX = int.Parse(Console.ReadLine());
-                        coordX--;
-
-                        Console.WriteLine("Choisir Colonne:");
-                        ascii = Encoding.ASCII.GetBytes(Console.ReadLine().ToLower());
-                        coordY = ascii[0] - 97;
-
-                        Console.WriteLine("Choisir Direction (Vertical = 0, Horizontal = 1):");
-                        direction = int.Parse(Console.ReadLine());
-
-                        if (!map.VerifyCoordOnMap(coordX, coordY, direction, size))
-                        {
-                            Console.WriteLine("Le navire ne peut pas être placé à ces coordonnées.");
-                        }
-                        else
-                        {
-                            break;
-                        }
-                    }
-                    catch (Exception)
-                    {
-                        Console.WriteLine("Coordonnées erronées. Recommencer.");
-                    }
-                }
-                
-
-                Ship newShip = new Ship(size, direction, nShip);
-                map.PutShipOnMap(newShip, coordX, coordY);
-
-                allShip.Add(newShip);
-                map.DisplayMap();
+                playerOne.DisplayEnemyMap();
+                playerOne.DisplayPlayerMap();
+                playerOne.AttackEnemyShip();
             }
         }
     }
