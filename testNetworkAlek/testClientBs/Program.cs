c424f6d265ff2ae2104ecbcf2d6cb655f321e2ce
Alek Mikolajek <alekmikopro@gmail.com>
2021-12-21 16:44:15 +0100

    Connexion client/serveur OK. Dossier à jout --> 'ClientOperationnel' & 'ServeurOperationnel'

---
+++ testNetworkAlek/testClientBs/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Sockets;
+using System;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace clienttest
+{
+
+
+}
+
+
+/* situtation au mardi 21/12 à 14:00 :
+connexion entre le client et le serveur établie.
+un message peut être envoyé au serveur (par le client)
+Le serveur peut recevoir le message. Le serveur ne peut pas répondre au message du client.*/
