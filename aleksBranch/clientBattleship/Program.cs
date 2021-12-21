aca58f4c23fbe561aa816d239253d97e9045c1cd
Alek Mikolajek <alekmikopro@gmail.com>
2021-12-21 21:56:44 +0100

    client/serveur opérationnel

---
+++ aleksBranch/clientBattleship/Program.cs
@@ -0,0 +1,93 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.IO;
+
+public class Client
+{
+    static void Main(string[] args)
+    {
+        //< Connect() >
+        Connect();
+    }
+
+    static void Connect()
+
+    {
+        connection:
+        try
+        {
+            //Utilise un point de terminaison distant pour établir une connexion par socket.
+            TcpClient tcpClient = new TcpClient();
+            IPAddress ipAddress = IPAddress.Parse("192.168.1.78");
+            IPEndPoint ipEndPoint = new IPEndPoint(ipAddress, 50000);
+
+
+            Console.WriteLine("Hello, bienvenue sur le serveur.");
+            tcpClient.Connect(ipEndPoint);
+            Console.WriteLine("\nVous êtes connecté !");
+
+
+            NetworkStream stream = tcpClient.GetStream();
+            StreamReader sr = new StreamReader(tcpClient.GetStream());
+            StreamWriter sw = new StreamWriter(tcpClient.GetStream());
+            /*
+             * 
+             * ENVOI TEXTE VERS LE SERVEUR 
+             * 
+             */
+            Console.WriteLine("saisir un message a envoyer");
+            string messageSend = Console.ReadLine();
+            int byteCount = Encoding.UTF8.GetByteCount(messageSend + 1);
+            byte[] dataSend = Encoding.UTF8.GetBytes(messageSend);
+
+
+
+
+            stream.Write(dataSend, 0, dataSend.Length); // methode qui envoi le message 
+
+            //Console.WriteLine(dataSend);
+            sw.WriteLine("BONJOUR");
+            sw.Flush();
+
+            sw.WriteLine("BONSOIR");
+            sw.Flush();
+
+            /*
+             * 
+             * RECEPTION TEXTE DEPUIS LE CLIENT 
+             * 
+             */
+            Byte[] dataRecieve = new byte[256];
+            string messageRecieve;
+
+            Int32 nbBytes = stream.Read(dataRecieve, 0, dataRecieve.Length);
+            messageRecieve = System.Text.Encoding.ASCII.GetString(dataRecieve, 0, nbBytes);
+            Console.WriteLine("Ce que j'ai recu : "+ messageRecieve);
+
+
+            messageSend = Console.ReadLine();
+            Byte[] dataSend2 = System.Text.Encoding.ASCII.GetBytes(messageSend);
+            sw.WriteLine(messageSend);
+            sw.Flush();
+
+            // sans çà pas d'envoyer ni réponse 
+            stream.Close();
+            tcpClient.Close();
+
+
+        }
+        catch (ArgumentNullException e)
+        {
+            Console.WriteLine("Test server");
+            Console.WriteLine("ArgumentNullException: {0}", e);
+        }
+        catch (SocketException e)
+        {
+            Console.WriteLine("SocketException: {0}", e);
+        }
+
+        Console.WriteLine("\n Press Enter to continue...");
+        Console.Read();
+    }
+}
---
+++ aleksBranch/servorBattleship/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class MyTcpListener
+{
+
+
+    public static void Main()
+    {
+        // Entrez dans la boucle d'écoute.
+        while (true)
+        {
+            TcpListener? server = null;
+            try
+            {
+                // Configurer le TcpListener sur le port 13000.
+                Int32 port = 50000;
+                IPAddress localAddr = IPAddress.Parse("192.168.1.78");
+
+                // TcpListener server = new TcpListener(port);
+                server = new TcpListener(localAddr, port);
+                // Commencez à écouter les demandes des clients..
+                server.Start();
+                // Tampon pour la lecture des données
+                Byte[] bytes = new Byte[256];
+                String? data = null;
+
+
+
+                Console.Write("Waiting for a connection... ");
+
+                // Effectuer un appel bloquant pour accepter les demandes.
+                // Vous pouvez également utiliser server.AcceptSocket() ici.
+                TcpClient client = server.AcceptTcpClient();
+                Console.WriteLine("Connected!");
+
+                data = "response";
+
+                // Obtenir un objet de flux pour la lecture et l'écriture
+                NetworkStream stream = client.GetStream();
+                StreamReader sr = new StreamReader(client.GetStream());
+                StreamWriter sw = new StreamWriter(client.GetStream());
+
+                Console.WriteLine("Bienvenue sur le serveur , le client est connecté");
+
+                int i;
+
+                // Boucle pour recevoir toutes les données envoyées par le client.
+
+                /*i = stream.Read(bytes, 0, bytes.Length);
+                Console.WriteLine("Received: {0}", sr.ReadLine());*/
+
+
+                while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                {
+                    // Translate data bytes to a ASCII string.
+                    data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                    Console.WriteLine("Received: {0}", data);
+
+                    // Process the data sent by the client.
+                    data = Console.ReadLine();
+
+                    byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+
+                    // Send back a response.
+                    stream.Write(msg, 0, msg.Length);
+                    Console.WriteLine("Sent: {0}", data);
+                }
+
+                Console.WriteLine("ici on test " + data);
+                byte[] msg2 = System.Text.Encoding.ASCII.GetBytes(data);
+                Console.WriteLine("test");
+                stream.Write(msg2, 0, msg2.Length);
+                Console.WriteLine(msg2);
+                // Arrêt et fin de connexion
+                client.Close();
+                Console.WriteLine("console fermé");
+
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+
+            finally
+            {
+                // Stop listening for new clients.
+#pragma warning disable CS8602 // Déréférencement d'une éventuelle référence null.
+                server.Stop();
+#pragma warning restore CS8602 // Déréférencement d'une éventuelle référence null.
+            }
+
+            Console.WriteLine("\nHit enter to continue...");
+            Console.Read();
+
+        } // while
+    }
+}
+
+//serveur diponible
